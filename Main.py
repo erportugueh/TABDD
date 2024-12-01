08ba34d043510752122b7391ce716d2d8580d8f9
erportugueh <118444589+erportugueh@users.noreply.github.com>
2024-12-01 14:04:19 +0000

    .

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 # main.py
+from Cart import Cart
 from DataBase import Database
-
 def display_menu():
     """Display the main menu."""
     print("\n--- Main Menu ---")
@@ -12,8 +12,13 @@ def display_user_menu(user):
     """Display the menu for logged-in users."""
     print(f"\n--- Welcome {user.username} ({user.role}) ---")
     print("1. Print My Details")
-    print("2. Logout and Return to Main Menu")
-    print("3. Exit")
+    print("2. Show Products")
+    print("3. Add Product to Cart")
+    print("4. Remove porduct from Cart")
+    print("5. View Cart")
+    print("6. Checkout")
+    print("7. Logout and Return to Main Menu")
+    print("8. Exit")
 
 def handle_login(db):
     username = input("Enter username: ")
@@ -37,6 +42,7 @@ def handle_register(db):
 
 def user_menu(db, user):
     """Display the user menu where logged-in users can interact with their profile."""
+    cart=Cart()
     while True:
         display_user_menu(user)
         choice = input("Enter your choice (1-3): ").strip()
@@ -54,17 +60,46 @@ def user_menu(db, user):
         elif choice == "2":
             print("\n--- Product List ---")
             db.display_products(user.role)
-        
+
         elif choice == "3":
+            try:
+                product_id = int(input("Enter Product ID to add: "))
+                quantity = int(input("Enter quantity: "))
+                product = db.get_product_by_id(product_id)
+                if product:
+                    cart.add_to_cart(product, quantity)
+                else:
+                    print("Invalid Product ID.")
+            except ValueError:
+                print("Invalid input. Please enter valid numbers.")
+        elif choice == "4":
+            try:
+                product_id = int(input("Enter Product ID to remove: "))
+                quantity = int(input("Enter quantity: "))
+                product = db.get_product_by_id(product_id)
+                if product:
+                    cart.remove_from_cart(product, quantity)
+                else:
+                    print("Invalid Product ID.")
+            except ValueError:
+                print("Invalid input. Please enter valid numbers.")
+        elif choice == "5":
+            cart.view_cart()
+        elif choice == "6":
+            cart.calculate_checkout()
+        
+        
+        elif choice == "7":
             print("Logging out and returning to the main menu.")
             break  # Exit the user menu and return to the main menu
         
-        elif choice == "4":
+        elif choice == "8":
             print("Exiting the system.")
             exit()  # Exit the entire program
         
         else:
             print("Invalid choice. Please try again.")
+
 
 def main():
     db = Database()
